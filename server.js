bfb270e83a68cdf8559b3950e692279dce8c1352
firman <firman@sagita.tech>
2020-06-01 08:50:34 +0700

    fix : cors error

--- server.js
+++ server.js
@@ -26,6 +26,11 @@ mongoose
 app.use(passport.initialize()); // Passport config
 require("./config/passport")(passport); // Routes
 app.use("/api/users", users);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "nutrigo.sagita.tech"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 const port = process.env.PORT || 5000;
 
